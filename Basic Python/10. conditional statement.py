0dc178f2bc29f708b7fbe1a06f81e1a10fd516ca
Geca981020 <78893099+Geca981020@users.noreply.github.com>
2021-02-18 12:53:48 +0900

    Add files via upload

---
+++ Basic Python/10. conditional statement.py
@@ -0,0 +1,35 @@
+"""
+1) if
+if expression :
+    statement(s)
+
+2) if-else
+if expression :
+    statement(s)
+else :
+    statement(s)
+
+3) if-elif-else
+if expression1 :
+   statement(s)
+elif expression2 :
+   statement(s)
+elif expression3 :
+   statement(s)
+else :
+   statement(s)
+
+4) nested if
+if expression1 :
+   statement(s)
+   if expression2 :
+      statement(s)
+   elif expression3 :
+      statement(s)
+   else
+      statement(s)
+elif expression4 :
+   statement(s)
+else:
+   statement(s)
+"""

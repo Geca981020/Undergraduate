f171d86745e657d1e6915aa02031060c8fbf1b94
Geca981020 <78893099+Geca981020@users.noreply.github.com>
2021-02-18 11:39:39 +0900

    Add files via upload

---
+++ Basic Python/03. variable.py
@@ -0,0 +1,7 @@
+x = 300
+y = 200
+name = "Hong gil dong"
+
+print(x+y)
+print(name)
+print(x, "와", y, "의 합은", x+y, "입니다.")
---
+++ Basic Python/05. arithmetic operator.py
@@ -0,0 +1,10 @@
+x = int(input("first number: "))
+y = int(input("second number: "))
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
